6748ed367e685058498a6eb88f7b74b6fbcb54e3
hedgehog319 <fign1910@mail.ru>
2025-07-07 15:34:31 +1000

    fix(tests): reverted .load() method again...

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,7 +12,7 @@ def test_fields_filling():
         p2: int = 0  # has default value
         p3: int | None  # is optional
 
-    s = S(file_names="tests/files/basic.toml")
+    s = S(setting_files="tests/files/basic.toml").load()
     assert s.p1 == 1
     assert s.p2 == 0
     assert s.p3 is UndefinedField
@@ -25,7 +25,7 @@ def test_no_require_field():
         p3: int  # must raise exception
 
     with pytest.raises(Exception):
-        S(file_names="tests/files/basic.toml")
+        S(setting_files="tests/files/basic.toml").load()
 
 
 def test_toml_types():
@@ -39,12 +39,12 @@ def test_toml_types():
         p_table: dict
 
     # shouldn't raise any error
-    S(file_names="tests/files/basic.toml")
+    S(setting_files="tests/files/basic.toml").load()
 
 
 def test_cusom_app_name():
     class S(Settings):
         p: int
 
-    s = S(app_name="custom_app_name", file_names="tests/files/basic.toml")
+    s = S(app_name="custom_app_name", setting_files="tests/files/basic.toml").load()
     assert s.p == 1
