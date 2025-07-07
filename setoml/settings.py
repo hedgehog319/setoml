ea3c3ffacd383a84e497ad733c517342cd6b6958
hedgehog319 <fign1910@mail.ru>
2025-07-07 15:36:35 +1000

    feat(core): significantly rewritten parser

--- setoml/settings.py
+++ setoml/settings.py
@@ -1,91 +1,118 @@
-from collections.abc import Iterable
+import itertools
 from pathlib import Path
-from typing import Any, TypeAlias
+from typing import Any
 
+from typing_extensions import Self
 
-from setoml.compat import toml_load
-from setoml.field import UndefinedField
+from setoml.exception import SettingNotExist
+from setoml.field import Field, UndefinedField
+from setoml.settings_source import PathLike, PathsLike, SettingsSource, TOMLSource
 from setoml.utils import (
     get_fields,
     is_optional,
-    is_subsettings,
+    is_subsetting,
 )
-
-FileNamesType: TypeAlias = Iterable[str] | str
 
 
 class Settings:
     _app_name: str | None
+    _setting_files: PathsLike
+    _secret_files: PathsLike
+    _settings_root: Path
 
-    _file_names: Iterable[Path]
-    _secret_names: Iterable[Path]
-
-    _skip_extras: bool
+    _settings_source: SettingsSource = TOMLSource()
 
     def __init__(
         self,
         app_name: str | None = None,
-        file_names: FileNamesType = "settings.toml",
-        secret_names: FileNamesType | None = None,
-        skip_extras: bool = True,
-        settings_root: str | None = None,
-        **kwargs: dict[str, Any],
+        setting_files: PathsLike | PathLike = None,
+        secret_files: PathsLike | PathLike = None,
+        settings_root: PathLike = Path.cwd(),
     ) -> None:
         self._app_name = app_name
-        if isinstance(file_names, str):
-            file_names = [file_names]
+        self._settings_root = Path(settings_root)
 
-        root = self.get_settings_root(settings_root)
-        self._file_names = list(map(lambda x: root / x, file_names))
-        self._secret_names = list(map(lambda x: root / x, secret_names or []))
-        self._skip_extras = skip_extras
+        self._setting_files = self._normalize_path(setting_files)
+        self._secret_files = self._normalize_path(secret_files)
 
-        settings = kwargs.get("_settings_dict", self._load_tomls(self._file_names))
-        if self._app_name:
-            settings = settings.get(self._app_name, {})
+    def __init_subclass__(cls, **kwargs) -> None:
+        cls._fields = tuple(f.name for f in get_fields(cls))
+
+    def __set_name__(self, _: type, name: str):
+        if not self._app_name:
+            self._app_name = name
+
+    def _normalize_path(self, files: PathsLike | PathLike) -> tuple[Path, ...] | None:
+        if files is None:
+            return None
+        if isinstance(files, (str, Path)):
+            return (self._settings_root / files,)
+        return tuple(self._settings_root / f for f in files)
+
+    def load(self, settings: dict[str, Any] | None = None) -> Self:
+        has_files = bool(self._setting_files or self._secret_files)
+        if settings is None and has_files:
+            settings = self._settings_source.load(
+                setting_files=self._setting_files,
+                secret_files=self._secret_files,
+            )
+
+        if settings and self._app_name:
+            if self._app_name not in settings:
+                setting_files = list(
+                    itertools.chain(self._setting_files or (), self._secret_files or ())
+                )
+
+                raise SettingNotExist(
+                    f"Settings for app `{self._app_name}` not found in files {setting_files}"
+                )
+            settings = settings[self._app_name]
+
+        if settings and has_files:
+            settings |= self._settings_source.load(
+                setting_files=self._setting_files,
+                secret_files=self._secret_files,
+            )
 
         self.model_validate(settings)
 
-    def get_settings_root(self, root: str | None) -> Path:
-        if not root:
-            return Path.cwd()
+        return self
 
-        return Path(root)
-
-    def __init_subclass__(cls) -> None:
-        cls._fields = tuple(f.name for f in get_fields(cls))
-
-    def _load_tomls(self, files: Iterable[Path]) -> dict[str, Any]:
-        settings = {}
-        for file in files:
-            settings.update(toml_load(file))
-        return settings
-
-    def model_validate(self, settings: dict):
+    def model_validate(self, settings):
         for field in get_fields(self):
             value = settings.get(field.name, field.default)
-            if is_subsettings(value, field.type) and field.default is UndefinedField:
-                setattr(
-                    self,
-                    field.name,
-                    field.type(_settings_dict=value),
-                )
 
+            if is_subsetting(value, field.type):
+                self._init_subsetting(value, field, settings)
             else:
-                if (
-                    not is_optional(field)
-                    and field.default is UndefinedField
-                    and field.name not in settings
-                ):
-                    raise Exception(f"Нет ключа {field.name}")
-                setattr(self, field.name, value)
+                self._init_required(value, field)
+
+    def _init_subsetting(self, value: Any, field: Field, settings: dict[str, Any]):
+        if isinstance(value, Settings):
+            setting = value.load(settings)
+        elif isinstance(value, dict):
+            if isinstance(field.default, Settings):
+                setting = field.default.load(settings)
+            else:
+                setting = field.type().load(value)
+                setting._app_name = field.name
+        elif value is UndefinedField:
+            setting = field.type().load()
+        else:
+            raise Exception(
+                f"Something went wrong in Settings._init_subsetting({value}, {field})"
+            )
+
+        setattr(self, field.name, setting)
+
+    def _init_required(self, value: Any, field: Field):
+        # CHECK: may be add field.default is UndefinedField
+        if value is UndefinedField and not is_optional(field):
+            raise Exception(
+                f"Setting not found {self.__class__.__qualname__}.{field.name}"
+            )
+        setattr(self, field.name, value)
 
     def __repr__(self) -> str:
-        text = (
-            f"{self.__class__.__qualname__}"
-            + "("
-            + ", ".join([f"{name}={getattr(self, name)!r}" for name in self._fields])
-            + ")"
-        )
-
-        return text
+        params = ", ".join([f"{name}={getattr(self, name)!r}" for name in self._fields])
+        return f"{self.__class__.__qualname__}({params})"
